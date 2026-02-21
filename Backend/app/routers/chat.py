2cb3e9d070051547827133c8c1bb063fb1396749
Davido_o <chan050516@gmail.com>
2026-02-21 23:33:33 +0800

    minor bug fix

--- Backend/app/routers/chat.py
+++ Backend/app/routers/chat.py
@@ -166,7 +166,7 @@ def grounding_search_agent(query: str) -> Dict[str, Any]:
     try:
         # ! Remove await to avoid error
         response = search_client.aio.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-3.0-flash-preview",
             contents=sub_agent_prompt,
             config=types.GenerateContentConfig(
                 tools=[types.Tool(google_search=types.GoogleSearch())],
