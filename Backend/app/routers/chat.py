dfa690d3b50d3d058547f60f4c97fd095631b94c
Davido_o <chan050516@gmail.com>
2026-02-24 14:01:22 +0800

    AI Model Change

--- Backend/app/routers/chat.py
+++ Backend/app/routers/chat.py
@@ -589,8 +589,7 @@ async def chat_with_document(request: ChatRequest, user_payload: dict = Depends(
         # 3. Start Chat
         # enable_automatic_function_calling=True for SDK to automatically handle Tool Functions Call
         chat = client.aio.chats.create(
-            # model="gemini-2.0-flash",
-            model="gemini-3-flash-preview",
+            model="gemini-2.5-pro",
             config=types.GenerateContentConfig(
                 tools=config["tools"],
                 system_instruction=config.get("prompt", ""),
